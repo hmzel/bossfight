5c03fc3198f8614f12ecf706ecd0505925a11438
hmzel <officiallucidus@gmail.com>
2023-12-05 17:59:52 -0600

    made the player get a set of items when they enter the world

--- src/main/java/me/zelha/bossfight/listeners/GeneralListener.java
+++ src/main/java/me/zelha/bossfight/listeners/GeneralListener.java
@@ -3,6 +3,8 @@ package me.zelha.bossfight.listeners;
 import me.zelha.bossfight.Bossfight;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,14 +12,34 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class GeneralListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
+        PlayerInventory inv = p.getInventory();
+        ItemStack bow = new ItemStack(Material.BOW);
 
+        inv.clear();
+        bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+        inv.addItem(new ItemStack(Material.DIAMOND_SWORD), bow);
+        inv.setItem(17, new ItemStack(Material.ARROW));
+        inv.setHelmet(new ItemStack(Material.IRON_HELMET));
+        inv.setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+        inv.setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+        inv.setBoots(new ItemStack(Material.IRON_BOOTS));
         p.teleport(new Location(Bukkit.getWorld("zelha"), 0.5, 27, 20.5, 180, 0));
         Bossfight.sendBossCreationPackets(p);
+
+        for (ItemStack item : inv.getArmorContents()) {
+            ItemMeta meta = item.getItemMeta();
+
+            meta.spigot().setUnbreakable(true);
+            item.setItemMeta(meta);
+        }
     }
 
     @EventHandler
